37c4ec4dd09faaa2841828e18d90988da56df04a
danikim9 <danikim9@gmail.com>
2026-09-09 12:46:47 +0900

    Add files via upload

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.shyftstarter.app',
+  appId: 'com.bellatrix.shyftstarter',
   appName: 'ShyftStarter',
   webDir: 'dist'
 };
